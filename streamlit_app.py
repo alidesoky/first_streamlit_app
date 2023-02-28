44606d17bbdcf1da49038a35e23cf6e389b4aaab
alidesoky <126562737+alidesoky@users.noreply.github.com>
2023-02-28 11:09:15 +0100

    Update streamlit_app.py
    subtitle

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,3 @@
 import streamlit
-streamlit.title('My Parents New Healthy Dinner')
-streamlit.subtitle('Breakfast Menu')
+streamlit.title('My Parents New Healthy Dinner'
+streamlit.subtitle 'Breakfast Menu')
