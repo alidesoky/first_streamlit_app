eff0ada247cb49ef9676e90b7bf94215b6af7c9d
alidesoky <126562737+alidesoky@users.noreply.github.com>
2023-02-28 11:13:56 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('omega 3 & blueberry oatmeal')
+streamlit.text('kale, spinach & Rocket Smoothie')
+streamlit.text(' hard_boiled free_range egg')
