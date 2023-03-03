0f3614ea25e27139f83dad2472721e2e9606395e
alidesoky <126562737+alidesoky@users.noreply.github.com>
2023-03-03 20:49:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,7 @@ try:
           else:
                               back_from_function = get_fruityvice_data(fruit_choice)
                               streamlit.dataframe (back_from_function)
-                              streamlit.stop()
+streamlit.stop()
 
 
 
